ef484e23295e06c811b8a33b96007c3ab64348ff
Nadir Hossain <nadirhossain336@gmail.com>
2024-02-01 19:13:25 +0600

    notifications toltip

--- src/components/Settings/Notifications.tsx
+++ src/components/Settings/Notifications.tsx
@@ -1,13 +1,21 @@
 const Notifications = () => {
   return (
     <div>
-      {[...Array(8)].map((index) => (
+      <div className="flex items-center justify-between">
+        <p className="text-[14px] font-semibold ">4 notifications</p>
+        <p className="text-[#0071E3] text-[10px] font-medium">Mark as read</p>
+      </div>
+      <div className="bg-[#0071E3] h-[1px] my-5 w-full"></div>
+      {[...Array(4)].map((index) => (
         <div
           key={index}
-          className="border border-[#d2aefd] py-2 mb-2 px-4 rounded-lg bg-[#eddeff6a] cursor-pointer hover:bg-[#ac6ef8a2] hover:text-white"
+          className="border flex gap-3 py-2 mb-2 px-4 rounded-lg cursor-pointer"
         >
-          <h2 className="text-md text-[#333333]">New customer register</h2>
-          <p className="text-[11px] text-[#A1A1A1]">10 minutes ago</p>
+          <div className="w-[12px] mt-[7px] h-[12px] rounded-full bg-[#0071E3]"></div>
+          <div>
+            <h2 className="text-[14px] text-[#333333]">New customer register</h2>
+            <p className="text-[8px] text-[#A1A1A1]">10 minutes ago</p>
+          </div>
         </div>
       ))}
     </div>
--- src/layouts/Main/Header.tsx
+++ src/layouts/Main/Header.tsx
@@ -1,6 +1,8 @@
 import { FiBell } from "react-icons/fi";
 import { Badge } from "antd";
 import { Link } from "react-router-dom";
+import { useState } from "react";
+import Notifications from "../../components/Settings/Notifications";
 
 interface HeaderProps {
   open: boolean;
@@ -8,16 +10,21 @@ interface HeaderProps {
 }
 
 const Header: React.FC<HeaderProps> = () => {
+  const [open, setOpen] = useState(false)
   return (
     <div className="flex items-center justify-between w-full">
       <div className="flex items-center gap-4">
-        <Link to="/settings/notifications" className="cursor-pointer">
-          <div className="relative w-[48px] flex items-center justify-center h-[48px] p-2 bg-white rounded-full">
-            <Badge style={{ background: "#0071E3", right: "-8px" }} count={5}>
-              <FiBell size={24} />
-            </Badge>
-          </div>
-        </Link>
+        <div onClick={()=>setOpen(!open)} className="relative cursor-pointer w-[48px] flex items-center justify-center h-[48px] p-2 bg-white rounded-full">
+          <Badge style={{ background: "#0071E3", right: "-8px" }} count={5}>
+            <FiBell size={24} />
+          </Badge>
+          {
+            open &&
+            <div className="p-4 absolute border border-[#0071E3] rounded-b-[16px] w-[251px] h-[312px] top-8 right-4 bg-white">
+              <Notifications/>
+            </div>
+          }
+        </div>
         <Link className="flex items-center gap-3" to="/settings/profile">
           <img
             src="https://raw.githubusercontent.com/mantinedev/mantine/master/.demo/avatars/avatar-8.png"
@@ -27,10 +34,14 @@ const Header: React.FC<HeaderProps> = () => {
           />
           <div>
             <h3 className="text-[#0071E3] font-medium text-[14px] text-center">Jane Cooper</h3>
-            <p className="text-right text-[14px]">Admin</p>
+            <p className="text-right font-semibold text-[14px]">Admin</p>
           </div>
         </Link>
       </div>
+
+      {
+
+      }
     </div>
   );
 };
