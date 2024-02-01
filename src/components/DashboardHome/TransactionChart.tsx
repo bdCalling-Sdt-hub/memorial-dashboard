250aff639a2b6de3aa50b3244041dd41b9e946a7
Nadir Hossain <nadirhossain336@gmail.com>
2024-02-01 16:51:48 +0600

    rechart modify

--- src/components/DashboardHome/TransactionChart.tsx
+++ src/components/DashboardHome/TransactionChart.tsx
@@ -1,53 +1,81 @@
-import { Bar, BarChart, ResponsiveContainer, Tooltip, XAxis } from "recharts";
+import { Bar, BarChart, ResponsiveContainer, XAxis } from "recharts";
 
 const data = [
   {
-    name: "Jan",
-    amount: 4000,
+    name: "1",
+    thisMonth: 3000,
+    lastMonth: 2500,
+
   },
   {
-    name: "Fab",
-    amount: 3000,
+    name: "2",
+    thisMonth: 4000,
+    lastMonth: 2300,
   },
   {
-    name: "Mar",
-    amount: 2000,
+    name: "3",
+    thisMonth: 3000,
+    lastMonth: 1000,
   },
   {
-    name: "Apr",
-    amount: 2780,
+    name: "4",
+    thisMonth: 2000,
+    lastMonth: 500,
   },
   {
-    name: "May",
-    amount: 2780,
+    name: "5",
+    thisMonth: 1000,
+    lastMonth: 700,
   },
   {
-    name: "Jun",
-    amount: 1890,
+    name: "6",
+    thisMonth: 1500,
+    lastMonth: 1000,
   },
   {
-    name: "July",
-    amount: 2390,
+    name: "7",
+    thisMonth: 1200,
+    lastMonth: 800,
   },
   {
-    name: "Aug",
-    amount: 3490,
+    name: "8",
+    thisMonth: 1600,
+    lastMonth: 700,
   },
   {
-    name: "Sep",
-    amount: 1890,
+    name: "9",
+    thisMonth: 1800,
+    lastMonth: 1200,
   },
   {
-    name: "Oct",
-    amount: 2390,
+    name: "10",
+    thisMonth: 3000,
+    lastMonth: 500,
   },
   {
-    name: "Nov",
-    amount: 2390,
+    name: "11",
+    thisMonth: 2000,
+    lastMonth: 1000,
   },
   {
-    name: "Dec",
-    amount: 3490,
+    name: "12",
+    thisMonth: 2000,
+    lastMonth: 1000,
+  },
+  {
+    name: "13",
+    thisMonth: 2000,
+    lastMonth: 1000,
+  },
+  {
+    name: "14",
+    thisMonth: 2000,
+    lastMonth: 1000,
+  },
+  {
+    name: "15",
+    thisMonth: 2000,
+    lastMonth: 1000,
   },
 ];
 
@@ -55,9 +83,9 @@ const TransactionChart = () => {
   return (
     <ResponsiveContainer width="100%" height={260}>
       <BarChart data={data} syncId="anyId">
-        <Tooltip />
         <XAxis dataKey="name" />
-        <Bar dataKey="amount" fill="#b278fb" />
+        <Bar radius={20} barSize={4.79} dataKey="thisMonth" fill="#8ABEF2" />
+        <Bar radius={20} barSize={4.79} dataKey="lastMonth" fill="#0071E3" />
       </BarChart>
     </ResponsiveContainer>
   );
