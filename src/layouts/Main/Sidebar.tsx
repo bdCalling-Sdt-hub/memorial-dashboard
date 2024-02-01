0919d4d77ea5d775599742e186e28e8e498be70a
Nadir Hossain <nadirhossain336@gmail.com>
2024-02-01 12:26:21 +0600

    modify sidebar

--- src/layouts/Main/Sidebar.tsx
+++ src/layouts/Main/Sidebar.tsx
@@ -1,14 +1,12 @@
-import {
-  IconApps,
-  IconCash,
-  IconClipboardList,
-  IconLogout2,
-  IconSettings,
-  IconUsers,
-  IconUsersGroup,
-} from "@tabler/icons-react";
+import { IconSettings } from "@tabler/icons-react";
+import { TbUsers } from "react-icons/tb";
+import { BiDollarCircle } from "react-icons/bi";
+import { TbCrown } from "react-icons/tb";
+import { PiBookOpenLight } from "react-icons/pi";
+import { MdOutlineSpaceDashboard } from "react-icons/md";
+import { HiLogout } from "react-icons/hi";
 import { Link, useLocation } from "react-router-dom";
-import logo from "../../assets/Logo.svg";
+import logo from "../../assets/Logo.png";
 
 interface SideProps {
   open: boolean;
@@ -21,32 +19,32 @@ const Sidebar: React.FC<SideProps> = ({ open }) => {
     {
       title: "Dashboard",
       path: "/",
-      icon: <IconApps size={20} />,
+      icon: <MdOutlineSpaceDashboard size={24} />,
     },
     {
       title: "Users",
       path: "/users",
-      icon: <IconUsers size={20} />,
+      icon: <TbUsers size={24} />,
     },
     {
-      title: "Transactions",
+      title: "Income",
+      path: "/income",
+      icon: <BiDollarCircle size={24} />,
+    },
+    {
+      title: "Subcription",
       path: "/transactions",
-      icon: <IconCash size={20} />,
+      icon: <TbCrown size={24} />,
     },
     {
-      title: "Transaction History",
+      title: "Stories",
       path: "/transaction-history",
-      icon: <IconClipboardList size={20} />,
-    },
-    {
-      title: "Workers",
-      path: "/workers",
-      icon: <IconUsersGroup size={20} />,
+      icon: <PiBookOpenLight size={24} />,
     },
     {
       title: "Settings",
       path: "/settings",
-      icon: <IconSettings size={20} />,
+      icon: <IconSettings size={24} />,
     },
   ];
 
@@ -54,18 +52,17 @@ const Sidebar: React.FC<SideProps> = ({ open }) => {
     <div
       className={`${
         open ? "col-span-1" : "col-span-2"
-      } bg-[#B278FB] h-[calc(100vh-40px)]  rounded-lg py-10 duration-300 transition-all`}
+      } bg-white text-[##0071E3] h-[calc(100vh-40px)]  rounded-lg py-10 duration-300 transition-all`}
     >
       <ul className="flex gap-3 flex-col h-full">
         <li className="mx-auto space-y-2 mb-5">
           <img src={logo} alt="logo" />
-          <h2 className="text-white text-lg">Russend</h2>
         </li>
         {linkItems.map((item, index) => (
           <li
             key={index}
-            className={`text-[15px] px-4 py-3 ${
-              item.path === pathname ? "bg-white text-violet-400" : "text-white"
+            className={`text-[18px] px-4 py-3 ${
+              item.path === pathname ? "bg-[#0071E3] text-white" : "text-[#0071E3]"
             } `}
           >
             <Link to={item.path} className="flex items-center gap-2">
@@ -74,12 +71,12 @@ const Sidebar: React.FC<SideProps> = ({ open }) => {
             </Link>
           </li>
         ))}
-        <li className="mt-auto px-4 py-4 bg-white text-violet-400 cursor-pointer text-md">
+        <li className="mt-auto mx-4 px-4 py-4 bg-white border border-[#0071E3] rounded-[8px]  cursor-pointer text-md">
           <Link to="/auth/login" className="flex items-center gap-2">
             <div className={`${open ? "mx-auto" : "m-0"}`}>
-              <IconLogout2 size={20} />
+              <HiLogout className="text-red-500" size={24} />
             </div>
-            <div className={`${open ? "hidden" : "block"}`}>Logout</div>
+            <div className={`${open ? "hidden" : "block"} text-[#0071E3]`}>Logout</div>
           </Link>
         </li>
       </ul>
