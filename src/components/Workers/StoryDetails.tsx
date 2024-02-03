369e4a22cfd817a2ba903e5cf89275c97ceabd86
Nadir Hossain <nadirhossain336@gmail.com>
2024-02-03 19:03:31 +0600

    story details

---
+++ src/components/Workers/StoryDetails.tsx
@@ -0,0 +1,38 @@
+import React from 'react'
+import Header from '../../layouts/Main/Header'
+import HeadingText from '../../util/HeadingText'
+import { RiArrowLeftSLine } from 'react-icons/ri'
+import { HiVolumeUp } from "react-icons/hi";
+import photo from "../../assets/Rectangle 178.png"
+
+const StoryDetails = () => {
+    return (
+        <div>
+            <div className="flex items-end justify-end mb-11">
+                <Header/>
+            </div>
+            <HeadingText color="#0071E3"> <RiArrowLeftSLine size={28} /> Story Details</HeadingText>
+
+            <div className='bg-white rounded-[16px] h-full p-6 mt-6'>
+                <div className='flex items-center justify-between'>
+                    <h1 className='text-6 font-semibold text-[#0071E3] mb-6'>We helped our father write his last story</h1>
+                    <div className='w-10 h-10 rounded-lg bg-[#0071E3] flex items-center justify-center'>
+                        <HiVolumeUp size={24} color='#FFFFFF' />
+                    </div>
+                </div>
+                <p className='text-[#2B2A2A] text-[18px] font-normal'>5:25 pm, 24 Dec, 2023</p>
+                <button className='w-[94px] h-[29px] rounded-[4px] bg-[#0071E3] text-white'>Indivisual</button>
+            
+
+                <div className='flex gap-2 mt-4 mb-[27px]'>
+                    <img src={photo} width={478} height={313} alt="" />
+                    <p className='text-[18px] font-normal'>Lorem ipsum dolor sit amet consectetur. Congue eget eget dignissim luctus placerat rutrum orci leo dignissim. Adipiscing volutpat nisi quis habitant platea. Ut eget suspendisse cursus a adipiscing. Orci rutrum nibh egestas scelerisque libero quis tincidunt dignissim. Ut nunc nunc tincidunt eget ac ullamcorper molestie pellentesque mollis. Suspendisse scelerisque eu quam malesuada rutrum. Sit egestas et turpis eget aliquet urna amet. Habitant feugiat eget purus nibh ullamcorper. Viverra accumsan amet euismod sed vitae tellus scelerisque.</p>
+                </div>
+
+                <p className='text-[18px] font-normal'>Lorem ipsum dolor sit amet consectetur. Congue eget eget dignissim luctus placerat rutrum orci leo dignissim. Adipiscing volutpat nisi quis habitant platea. Ut eget suspendisse cursus a adipiscing. Orci rutrum nibh egestas scelerisque libero quis tincidunt dignissim.</p>
+            </div>
+        </div>
+    )
+}
+
+export default StoryDetails
--- src/components/Workers/UserStory.tsx
+++ src/components/Workers/UserStory.tsx
@@ -1,3 +1,4 @@
+import { Link } from "react-router-dom";
 import photo from "../../assets/Rectangle.png";
 import { Pagination } from 'antd';
 const UserStory = () => {
@@ -15,14 +16,16 @@ const UserStory = () => {
             <div className="grid grid-cols-2 gap-4 my-6">
         {
           [...Array(10)].map((index)=>(
-            <div key={index} className="flex items-center gap-6 w-full bg-white rounded-lg p-2">
-                <img src={photo} width={100} height={100} alt="" />
-                <div>
-                  <h3 className="text-[18px] font-medium">We helped our father write his last story</h3>
-                  <h4 className="text-[14px] font-normal">5:25 pm</h4>
-                  <h4 className="text-[14px] font-normal">24 Dec, 2023</h4>
-                </div>
-            </div>
+            <Link key={index} to="/workers/story-details">
+              <div  className="flex items-center gap-6 w-full bg-white rounded-lg p-2">
+                  <img src={photo} width={100} height={100} alt="" />
+                  <div>
+                    <h3 className="text-[18px] font-medium">We helped our father write his last story</h3>
+                    <h4 className="text-[14px] font-normal">5:25 pm</h4>
+                    <h4 className="text-[14px] font-normal">24 Dec, 2023</h4>
+                  </div>
+              </div>
+            </Link>
           ))
         }
       </div>
